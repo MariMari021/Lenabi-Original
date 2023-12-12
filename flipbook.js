ef79e937114e1ed6ce647dc75c5598bca58aaae1
MariMari021 <mariana.gsilva0021@gmail.com>
2023-12-12 10:29:48 -0300

    okok

--- flipbook.js
+++ flipbook.js
@@ -1,18 +1,24 @@
-//turn pages when click next or prev button
+//vira a pagina quando clica nos botoes next e prev
 const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
 
 pageTurnBtn.forEach((el, index) => {
+    // função de clique para cada botao
     el.onclick = () => {
+        // obtem o valor do atributo data-page do botão, que indica a página associada ao botão.
         const pageTurnId = el.getAttribute('data-page');
         const pageTurn = document.getElementById(pageTurnId);
 
+        // se a classe turn estiver presente no elemento
         if (pageTurn.classList.contains('turn')) {
+            // ela é removida
             pageTurn.classList.remove('turn');
+            // temporizador que define o estilo zIndex do elemento da página que se ajusta com base no índice do botão.
             setTimeout(() => {
                 pageTurn.style.zIndex = 20 - index;
             }, 500)
         }
         else {
+            // se nao, a classe turn é acionada
             pageTurn.classList.add('turn');
             setTimeout(() => {
                 pageTurn.style.zIndex = 20 + index;
@@ -21,19 +27,23 @@ pageTurnBtn.forEach((el, index) => {
     }
 })
 
-//contact me button when click
+//função para executar os botoes
 const pages = document.querySelectorAll('.book-page.page-right');
 const contactMeBtn = document.querySelector('.btn.end');
 
 contactMeBtn.onclick = () => {
+    // itera cada page no conjuto de paginas selecionada
     pages.forEach((page, index) => {
+        // temporizador para que cada pagina vire em momento especifico 
         setTimeout(() => {
             page.classList.add('turn');
 
+            // atraso de um segundo para alterar a propriedade zIndex, controlando a ordem de empilhamento das páginas para garantir que fiquem sobrepostas corretamente
             setTimeout(() => {
                 page.style.zIndex = 20 + index;
             }, 500)
             
+            //  tempo de atraso é calculado dinamicamente com base no índice da página
         }, (index +1) * 200 + 100)
     })
 }
@@ -52,6 +62,7 @@ function reserveIndex() {
 //back profile button when clock 
 const backProfileBtn = document.querySelector('.back-profile');
 
+// clicar no botao a pagina vokta para tras
 backProfileBtn.onclick = () => {
     pages.forEach((_, index) => {
         setTimeout( () => {
